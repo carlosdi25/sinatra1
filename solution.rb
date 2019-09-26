e4ec60f441531c5238fa6431cffb662d4019280c
Carlos Diaz <51090663+carlosdi25@users.noreply.github.com>
2019-09-26 06:40:42 -0500

    2

--- solution.rb
+++ solution.rb
@@ -3,11 +3,11 @@ require 'sinatra'
 get '/' do
     unless params[:nombre]
         <<-HTML
-        <H1>Hola Desconocido<H1>
+        <H1>Hola Desconocido!<H1>
         HTML
     else
         <<-HTML
-        <H1>Hola #{params[:nombre]}<H1>
+        <H1>Hola #{params[:nombre]}!<H1>
         HTML
     end
 end
