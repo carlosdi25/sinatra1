d8ea319276d555cff4247659f040897ed0cd6484
Carlos Diaz <51090663+carlosdi25@users.noreply.github.com>
2019-09-26 06:36:53 -0500

    1

---
+++ solution.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+get '/' do
+    unless params[:nombre]
+        <<-HTML
+        <H1>Hola Desconocido<H1>
+        HTML
+    else
+        <<-HTML
+        <H1>Hola #{params[:nombre]}<H1>
+        HTML
+    end
+end
